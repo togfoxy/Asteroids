9cf4795f326aa93dab0cc02f5d0bbc9a2d4a413c
Fox <g_t_campbell@yahoo.com.au>
2022-08-11 10:57:04 +1000

    Fixes #150

--- Source/keymaps.lua
+++ Source/keymaps.lua
@@ -7,10 +7,10 @@ function keymaps.init()
             moveleft = {'key:kp4', 'key:a'},
             moveright = {'key:kp6', 'key:d'},
             moveforward = {'key:kp8', 'key:w'},
-            movebackwards = {'key:kp2', 'key:x'},
+            movebackwards = {'key:kp2', 'key:s'},
             rotateleft = {'key:kp7', 'key:q'},
             rotateright = {'key:kp9', 'key:e'},
-            centreview = {'key:kp5', 'key:s'}
+            centreview = {'key:kp5', 'key:c'}
     }
 }
 
